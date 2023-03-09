14b291d9ee7b32b939d63b460632e31a1b076e6c
kishor-saresa <kishor1392@tnraocollege.org>
2023-03-09 16:17:41 +0530

    forth

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-
+//  Crud operations
+//  global variable
 function Submit(){
-    alert("Submited Successfully")
+  var dataEntered = retrieveData();
+  var readdata = readingDataFromLocalStorage(dataEntered);
+  console.log(readdata);
 }
-function Reset(){
-    alert("Reset Successfully")
+// retrive data in form
+function retrieveData(){
+    var name =document.getElementById("Name").value;
+    var email =document.getElementById("Email").value;
+
+    var arr=[name,email];
+    return arr;
+}
+// data in localStorage
+function readingDataFromLocalStorage(dataEntered){
+    var n = localStorage.setItem("Name",dataEntered[0]);
+    var e = localStorage.setItem("Email",dataEntered[1]);
+    
+    // getting value in table
+    var n1 =localStorage.getItem("Name",n);
+    var e1 = localStorage.getItem("Email",e);
+    
+    var arr=[n1,e1];
+    return arr;
 }
