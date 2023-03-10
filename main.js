12291fa1b22c41670632840c14953f8e5db88b53
kishor-saresa <kishor1392@tnraocollege.org>
2023-03-10 17:11:19 +0530

    addjs

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 //  Crud operations
-//  global variable
+
 function Submit(){
   var dataEntered = retrieveData();
-  var readdata = readingDataFromLocalStorage(dataEntered);
-  console.log(readdata);
+  var readData = readingDataFromLocalStorage(dataEntered);
+  
+  
+    insert(readData);
+    msg.innerHTML="data inserted"; 
+    update();
+    msg.innerHTML="data updated";
 }
 // retrive data in form
 function retrieveData(){
@@ -13,6 +18,7 @@ function retrieveData(){
     var arr=[name,email];
     return arr;
 }
+// Read
 // data in localStorage
 function readingDataFromLocalStorage(dataEntered){
     var n = localStorage.setItem("Name",dataEntered[0]);
@@ -21,7 +27,38 @@ function readingDataFromLocalStorage(dataEntered){
     // getting value in table
     var n1 =localStorage.getItem("Name",n);
     var e1 = localStorage.getItem("Email",e);
+    var ph=localStorage.getItem("Phone number",ph)
+    var dob=localStorage.getItem("Birthdate",dob)
     
     var arr=[n1,e1];
     return arr;
+}
+// Insert
+function  insert(readData)
+{
+    var row = table.insertRow();
+    row.insertCell(0).innerHTML =readData[0];
+    row.insertCell(1).innerHTML =readData[1];
+    row.insertCell(2).innerHTML =readData[2];
+    row.insertCell(3).innerHTML = `<button onclick=edit(this)>Edit</button>
+                                    <button onclick=remove(this)>Delete</button>`;
+
+    // Edit 
+    function edit(td){
+         row= td.parentElement.prentElement;
+        document.getElementById("Name").value=row.cells[0].innerHTML;
+        document.getElementById("Email").value=row.cells[1].innerHTML;
+    }
+    // Updaet  
+    function update(){
+        row.cells[0].innerHTML=document.getElementById("Name").value;
+        row.cells[1].innerHTML=document.getElementById("Email").value;
+         row =null;
+
+    } 
+    // Delete
+    function remove(td){
+        row= td.parentElement.parentElement;
+        document.getElementById("table").deleteRow(row.rowIndex);
+    }                         
 }
